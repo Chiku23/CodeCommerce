e23c5cc592a4cb8a9767f6ff18da9f5ee22d3d47
Chiku23 <subhakantacs2000@gmail.com>
2025-05-24 01:11:33 +0530

    Setup TailwindCSS, Routes, Controllers and basic layout.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use app\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// include the admin routes
+require_once('./admin.php');
+
+/*
+ ********************** 
+ * Define All Routes.
+ **********************
+*/
+
+// Home Route
+Route::get('/', [HomeController::class , 'index'])->name('home');
+
+// Auth Routes
+Route::get('login', [LoginController::class , 'login'])->name('login');
+Route::get('register', [RegisterController::class , 'register'])->name('register');
