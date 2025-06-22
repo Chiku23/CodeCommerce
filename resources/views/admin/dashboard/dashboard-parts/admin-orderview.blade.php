104e35fdce44b9756fc5c8a06b9e36d710aaeb09
Chiku <100275109+Chiku23@users.noreply.github.com>
2025-06-22 19:22:27 +0530

    Update resources/views/admin/dashboard/dashboard-parts/admin-orderview.blade.php
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- resources/views/admin/dashboard/dashboard-parts/admin-orderview.blade.php
+++ resources/views/admin/dashboard/dashboard-parts/admin-orderview.blade.php
@@ -93,7 +93,7 @@
                     </tr>
                     <tr>
                         <td class="{{ $td1Class }} {{ $tdBorderClass }}">
-                            Tracing Number
+                            Tracking Number
                         </td>
                         <td class="{{ $tdClass }} {{ $tdBorderClass }}">
                             {{ $objOrder->tracking_number ?? 'N/A' }}
